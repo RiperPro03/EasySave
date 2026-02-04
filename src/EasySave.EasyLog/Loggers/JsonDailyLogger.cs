b172b8f5911c23c88abf89cfe0ee2e2248986f32
shayna <shaynarsr8@gmail.com>
2026-02-04 16:21:03 +0100

    ajout interface + début dailylogger

---
+++ src/EasySave.EasyLog/Loggers/JsonDailyLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasySave.EasyLog.Interfaces;
+
+namespace EasySave.EasyLog.Loggers
+{
+    public class JsonDailyLogger<T> : ILogger<T>
+    {
+        private readonly string logDirectory;
+
+        public JsonDailyLogger(string logDirectory)
+        {
+            this.logDirectory = logDirectory;
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+
+        public bool Write(T entry)
+        {
+           
+        }
+    }
+}
