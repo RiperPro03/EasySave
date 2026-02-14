db3f9ed1d846364c28be543da864719c8da6ce68
shayna <shaynarsr8@gmail.com>
2026-02-14 19:24:01 +0100

    update

--- src/EasySave.App/Services/BackupEngine.cs
+++ src/EasySave.App/Services/BackupEngine.cs
@@ -203,7 +203,7 @@ internal sealed class BackupEngine : IBackupEngine
                         var logEntry = LogEntryBuilder.Create(
                             eventName: "file.encrypted",
                             category: LogEventCategory.File,
-                            action: LogEventAction.Copy,
+                            action: LogEventAction.Unknown,
                             message: $"File {Path.GetFileName(sourcePath)} encrypted")
                         .WithFile(
                             sourcePath: sourcePath,
